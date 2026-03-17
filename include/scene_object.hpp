e2e22725c29d36b37772aec954015c5c553bc6eb
clr <clrcera05@gmail.com>
2026-03-17 17:01:22 -0300

    Scene Object transformation matrixes

--- include/scene_object.hpp
+++ include/scene_object.hpp
@@ -8,14 +8,31 @@ class SceneObject {
 public:
   SceneObject(std::string path);
 
-  glm::vec3 rotation;
-  glm::vec3 scale;
-  glm::vec3 position;
   Mesh mesh;
 
   void draw();
 
   glm::mat4 transMatrix();
+
+  void rotate(glm::vec3 rot);
+  void scaleUp(glm::vec3 sca);
+  void translate(glm::vec3 tra);
+
+  void setRotation(glm::vec3 rot);
+  void setScale(glm::vec3 sca);
+  void setPosition(glm::vec3 tra);
+
+  glm::vec3 getRotation();
+  glm::vec3 getScale();
+  glm::vec3 getPosition();
+
+private:
+  glm::mat4 cachedTransMatrix;
+  bool transMatrixNeedsUpdate = true;
+
+  glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
+  glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
 };
 
 #endif // SCENE_OBJECT_HPP
--- src/scene_object.cpp
+++ src/scene_object.cpp
@@ -1,1 +1,58 @@
 #include "scene_object.hpp"
+#include "glm/ext/matrix_transform.hpp"
+
+SceneObject::SceneObject(std::string path) : mesh(path) {}
+
+// Transformation matrix caching, every time a transformation is updated,
+// the matrix is marked as dirty and will be recalculated on the next call to
+// transMatrix()
+glm::mat4 SceneObject::transMatrix() {
+  if (transMatrixNeedsUpdate) {
+    glm::mat4 transMatrix = glm::mat4(1.0f);
+    transMatrix = glm::translate(transMatrix, position);
+
+    transMatrix = glm::rotate(transMatrix, glm::radians(rotation.x),
+                              glm::vec3(1.0f, 0.0f, 0.0f));
+    transMatrix = glm::rotate(transMatrix, glm::radians(rotation.y),
+                              glm::vec3(0.0f, 1.0f, 0.0f));
+    transMatrix = glm::rotate(transMatrix, glm::radians(rotation.z),
+                              glm::vec3(0.0f, 0.0f, 1.0f));
+    transMatrix = glm::scale(transMatrix, scale);
+
+    cachedTransMatrix = transMatrix;
+    transMatrixNeedsUpdate = false;
+  }
+  return cachedTransMatrix;
+}
+
+// Incremental transformations
+void SceneObject::rotate(glm::vec3 rot) {
+  rotation += rot;
+  transMatrixNeedsUpdate = true;
+}
+void SceneObject::scaleUp(glm::vec3 sca) {
+  scale += sca;
+  transMatrixNeedsUpdate = true;
+}
+void SceneObject::translate(glm::vec3 tra) {
+  position += tra;
+  transMatrixNeedsUpdate = true;
+}
+
+// Directly set transformations
+void SceneObject::setRotation(glm::vec3 rot) {
+  rotation = rot;
+  transMatrixNeedsUpdate = true;
+}
+void SceneObject::setScale(glm::vec3 sca) {
+  scale = sca;
+  transMatrixNeedsUpdate = true;
+}
+void SceneObject::setPosition(glm::vec3 tra) {
+  position = tra;
+  transMatrixNeedsUpdate = true;
+}
+
+glm::vec3 SceneObject::getRotation() { return rotation; }
+glm::vec3 SceneObject::getScale() { return scale; }
+glm::vec3 SceneObject::getPosition() { return position; }
