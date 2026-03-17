9138e8c565bf285bd8a34743166a7a6a8b451320
clr <clrcera05@gmail.com>
2026-03-17 19:16:02 -0300

    fix mesh AttribPointer

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -2,48 +2,51 @@
 
 #include <iostream>
 
-
-
 Mesh::Mesh(std::string path_to_wavefront_obj) {
-    // Parse wavefront object
-    auto parser = ObjFileParser(path_to_wavefront_obj);
-    auto [vertices, indices] = parser.parse();
-    this->vertices = vertices;
-    this->indices = indices;
+  // Parse wavefront object
+  auto parser = ObjFileParser(path_to_wavefront_obj);
+  auto [vertices, indices] = parser.parse();
+  this->vertices = vertices;
+  this->indices = indices;
+  for (const auto &vertex : vertices) {
+    std::cout << "Vertex: (" << vertex.position.x << ", " << vertex.position.y
+              << ", " << vertex.position.z << ")\n";
+    std::cout << "Color: (" << vertex.color.x << ", " << vertex.color.y << ", "
+              << vertex.color.z << ")\n";
+  }
 
-    // Setup gpu vertex buffers
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &EBO);
+  // Setup gpu vertex buffers
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
 
-    // Object VAO bound
-    glBindVertexArray(VAO);
+  // Object VAO bound
+  glBindVertexArray(VAO);
 
-    // Vertex buffer bound and filled
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+  // Vertex buffer bound and filled
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex),
+               vertices.data(), GL_STATIC_DRAW);
 
-    // Element buffer bound and filled
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+  // Element buffer bound and filled
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint),
+               indices.data(), GL_STATIC_DRAW);
 
-    // Setup buffer attributes
+  // Setup buffer attributes
 
-    // position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float), (void*)0);
-    // color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
+  // position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+  // color
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
 
-    // Unbinds VAO
-    glBindVertexArray(0);
+  // Unbinds VAO
+  glBindVertexArray(0);
 }
 
-void Mesh::bind() {
-    glBindVertexArray(VAO);
-}
+void Mesh::bind() { glBindVertexArray(VAO); }
 
-void Mesh::unbind() {
-    glBindVertexArray(0);
-}
+void Mesh::unbind() { glBindVertexArray(0); }
