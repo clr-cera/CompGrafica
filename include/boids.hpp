0ac0402139a954a006b85594bfbed63dec21b226
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2026-04-30 20:42:55 -0300

    Fix boid parameters

--- include/boids.hpp
+++ include/boids.hpp
@@ -10,27 +10,27 @@
 // ========================================================================
 
 // Boid spawning
-constexpr int BOID_COUNT = 30;
-constexpr std::pair<float, float> BOID_SPAWN_XRANGE = {-3.0f, 3.0f};
-constexpr std::pair<float, float> BOID_SPAWN_YRANGE = {0.2f, 1.0f};
-constexpr std::pair<float, float> BOID_SPAWN_ZRANGE = {-3.0f, 3.0f};
+constexpr int BOID_COUNT = 35;
+constexpr std::pair<float, float> BOID_SPAWN_XRANGE = {-4.0f, 4.0f};
+constexpr std::pair<float, float> BOID_SPAWN_YRANGE = {2.2f, 3.4f};
+constexpr std::pair<float, float> BOID_SPAWN_ZRANGE = {-4.0f, 4.0f};
 constexpr std::pair<float, float> BOID_SPAWN_VEL_RANGE = {-0.01f, 0.01f};
-constexpr float BOID_SCALE = 0.01f;
+constexpr float BOID_SCALE = 0.02f;
 
 // Simulation constraints
 constexpr float BOID_MAX_SPEED = 0.05f;
-constexpr float BOID_INNER_DISTANCE = 0.2f;
-constexpr float BOID_OUTER_DISTANCE = 1.2f;
+constexpr float BOID_INNER_DISTANCE = 0.5f;
+constexpr float BOID_OUTER_DISTANCE = 1.7f;
 
 // Position bounds
-constexpr std::pair<float, float> BOID_BOUNDS_X = {-3.0f, 3.0f};
-constexpr std::pair<float, float> BOID_BOUNDS_Y = {0.2f, 1.0f};
-constexpr std::pair<float, float> BOID_BOUNDS_Z = {-3.0f, 3.0f};
+constexpr std::pair<float, float> BOID_BOUNDS_X = {-4.0f, 4.0f};
+constexpr std::pair<float, float> BOID_BOUNDS_Y = {2.2f, 3.4f};
+constexpr std::pair<float, float> BOID_BOUNDS_Z = {-4.0f, 4.0f};
 
 // Forces
 constexpr float BOID_ATTRACTION_FORCE = 0.008f;
-constexpr float BOID_REPULSION_FORCE = 0.008f;
-constexpr float BOID_ALIGN_FORCE = 0.5f;
+constexpr float BOID_REPULSION_FORCE = 0.01f;
+constexpr float BOID_ALIGN_FORCE = 1.0f;
 
 // Adds objects to the scene
 void spawn_boids(Scene *scene, std::string obj_path, std::string texture_path);
